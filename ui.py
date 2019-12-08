ece58f451bd06b0d5024703f6f8f0cb2fb4131b4
smbh <smb_h@yahoo.com>
2019-12-08 22:39:40 +0330

    working on finding nearby points

--- ui.py
+++ ui.py
@@ -18,8 +18,20 @@ def draw_points(point_locs):
     y_list = []
     for point in point_locs:
         y_list.append(point[1])
+    condition_list = []
+    for point in point_locs:
+        stat = point[2]
+        if stat == 1:
+            condition_list.append("r")
+        elif stat == 2:
+            condition_list.append("g")
+        else:
+            condition_list.append("b")
+
     # Draw point based on above x, y axis values.
-    plt.scatter(x_list, y_list, s=15)
+    # plt.scatter(x_list, y_list, s=15, c="b")
+    for indx in range(len(x_list)):
+        plt.scatter(x_list[indx], y_list[indx], s=15, c=condition_list[indx])
     # Set chart title.
     plt.title("people", fontsize=19)
     # Set x axis label.
