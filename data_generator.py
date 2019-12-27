dece431fec117ec51ee332b90ab29bafe5ecb83d
smbh <smb_h@yahoo.com>
2019-12-27 21:48:47 +0330

    wrapping up

--- data_generator.py
+++ data_generator.py
@@ -14,12 +14,12 @@ data_list = []
 for i in range(lanes):
     x = random.randint(0, 100000)
     y = random.randint(0, 100000)
-    data_list.append([x, y, 0])
+    data_list.append([x, y, 0, "b"])
 
 
 # infect 1
 infect_indx =random.randint(0, lanes)
-data_list[infect_indx][2] = 1
+data_list[infect_indx][3] = "r"
 
 
 data = pd.DataFrame(data_list)
--- ui.py
+++ ui.py
@@ -1,32 +1,48 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from time import sleep
+from random import randrange
 
 
 data = []
 df = pd.read_csv("data.csv")
 for index, row in df.iterrows():
-    data.append([row[1], row[2], row[3]])
+    data.append([row[1], row[2], int(row[3]), row[4]])
+
+data.sort()
+
+# find neighbor points
+def find_neighbors(x, y, point_locs):
+    # calculate with radius of 3000
+    rs = []
+    r = 3000
+    for point in point_locs:
+        x_diff_range = [abs(x - r), abs(x + r)]
+        y_diff_range = [abs(y - r), abs(y + r)]
+        if (x_diff_range[0] < point[0] < x_diff_range[1]) and (y_diff_range[0] < point[1] < y_diff_range[1]) :
+            rs.append(point)
+    print("#########################################################")
+    print(rs)
+    return rs
 
 
 # Draw a point based on the x, y axis value.
 def draw_points(point_locs):
     # x axis value list.
     x_list = []
-    for point in point_locs:
-        x_list.append(point[0])
     # y axis value list.
     y_list = []
-    for point in point_locs:
-        y_list.append(point[1])
+    # time list
+    time_list = []
+    # condition list - red as infected, green for cured, blue for normal
     condition_list = []
     for point in point_locs:
-        stat = point[2]
-        if stat == 1:
-            condition_list.append("r")
-        elif stat == 2:
-            condition_list.append("g")
-        else:
-            condition_list.append("b")
+        x_list.append(point[0])
+        y_list.append(point[1])
+        time_list.append(point[2])
+        condition_list.append(point[3])
+        # find closest to infected
+
 
     # Draw point based on above x, y axis values.
     # plt.scatter(x_list, y_list, s=15, c="b")
@@ -43,9 +59,50 @@ def draw_points(point_locs):
     # input("press any key to continue")
 
 
+# Update time
+def update_time(point_locs):
+    for point in point_locs:
+        point[2] += 1
+        stat = point[3]
+        # cure after 3 sec of infection
+        if stat == "r" and point[2] >= 3:
+            p = randrange(100)
+            # 95% chance to cure
+            if p < 95:
+                point[3] = "g"
+                point[2] = 0
+        # infect
+        elif stat == "r":
+            p = randrange(100)
+            # 95% chance to infect
+            if p < 95:
+                neighbors = find_neighbors(point[0], point[1], point_locs)
+                if neighbors:
+                    for ng in neighbors:
+                        ng_stat = ng[3]
+                        p2 = randrange(100)
+                        # 99% to infect
+                        if ng_stat == "b" and p2 < 99:
+                            ng[3] = "r"
+                            ng[2] = 0
+                        # 1% chance to infect cured people
+                        elif (p2 < 1) and ng_stat == "g":
+                            ng[3] = "r"
+                            ng[2] = 0
+
+    draw_points(point_locs)
+
+
+
 
 def main():
     draw_points(data)
+    # Time to live
+    for t in range(20):
+        sleep(.1)
+        update_time(data)
+
+
 
 
 if __name__ == '__main__':
